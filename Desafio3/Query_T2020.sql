6a6e105c32b81ddf44634fffc1a8618ac84ef242
LeonardoRCosta <rm94286@fiap.com.br>
2022-06-03 17:11:18 -0300

    Arquivos dos desafios 1-3 add.

---
+++ Desafio3/Query_T2020.sql
@@ -0,0 +1,13 @@
+use desafio3;
+CREATE table T20201(
+cd_conta int NOT NULL,
+dt_registro date NOT NULL,
+nr_registro int NOT NULL,
+ds_procedimento varchar(255) NOT NULL,
+vl_saldo varchar(255) NOT NULL,
+primary key(cd_conta)
+);
+
+
+
+
